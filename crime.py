41fd8c2b8ed2c36a05f38ca312f6f44bb14e468d
Nisarg <nisarg.gajjar@dal.ca>
2017-10-02 12:49:43 -0300

    debugging

--- crime.py
+++ crime.py
@@ -1,7 +1,26 @@
+import string
+#asslt=0
 import fileinput
-with open('Crime.txt') as f:
+with open('Crime.csv') as f:
 	data = f.readlines()
 	for line in data:
-		 words = line.split()
-	for word in words:
-		print(word)
+		words = line.split()
+		#for word in words:
+		#	global asslt
+		print(words)
+		count=words.count("ASSAULT")
+		print(count)
+			#xy
+			#asslt=0
+"""			if word=='ASSAULT':
+				asslt += 1 
+			elif word=='THEFT FROM VEHICLE':
+				theft_vehicle+=1
+			elif word=='BREAK AND ENTER':
+				breaknenter+=1
+			elif word=='ROBBERY':
+				robbery+=1
+			elif word=='THEFT OF VEHICLE':
+				tov+=1
+			#print(asslt)
+"""
