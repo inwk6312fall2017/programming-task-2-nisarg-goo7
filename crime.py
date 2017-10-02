6edfc845c4b9e7bd3407ae9ed7eeb00db3a2831c
Nisarg <nisarg.gajjar@dal.ca>
2017-10-02 12:09:36 -0300

    fixed wrong indentation error

--- crime.py
+++ crime.py
@@ -1,7 +1,7 @@
 import fileinput
 with open('Crime.txt') as f:
-        data = f.readlines()
-        for line in data:
-                words = line.split()
+	data = f.readlines()
+	for line in data:
+		 words = line.split()
 	for word in words:
 		print(word)
